de579aaf7d5fb3ecb675fe4cd8814e6bdf8aa49e
Adaptist <adaptist1@gmail.com>
2023-04-25 15:38:14 +0300

    6.09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const widgetBtn = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+function changeColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  spanColor.textContent = randomColor;
+}
+
+widgetBtn.addEventListener('click', changeColor);
