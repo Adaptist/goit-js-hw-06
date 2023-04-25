5ecf872410fe76e69de98b6c53338f0d786ac9c2
Adaptist <adaptist1@gmail.com>
2023-04-25 16:47:18 +0300

    6.10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxForDiv = document.createElement('div');
+
+document.body.append(boxForDiv);
+
+function createBoxes(amount) {
+
+let boxSize = 30;
+
+  for (let index = 0; index < Number(amount); index++) {
+    const elementDiv = document.createElement('div');
+
+    elementDiv.style.backgroundColor = getRandomHexColor();
+    elementDiv.style.width = `${boxSize}px`;
+    elementDiv.style.height = `${boxSize}px`;
+
+    boxSize += 10;
+
+    boxForDiv.append(elementDiv);
+  }
+}
+
+function destroyBoxes() {
+  boxForDiv.innerHTML = '';
+}
+
+function onCreate() {
+  if (input.value === '') return;
+  createBoxes(input.value);
+  input.value = '';
+}
+
+destroyBtn.addEventListener('click', destroyBoxes);
+createBtn.addEventListener('click', onCreate);
