f0610b2dbf5a0847e2beef9cfa14c0ab07a4efe6
Adaptist <adaptist1@gmail.com>
2023-04-25 17:39:15 +0300

    6.05 fixed

--- js/task-05.js
+++ js/task-05.js
@@ -3,8 +3,5 @@ const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
 nameInput.addEventListener('input', onInput => {
-    nameOutput.textContent = 
-    nameInput.value ? 
-    nameInput.value :
-    'Anonymous';
+    nameOutput.textContent = nameInput.value || 'Anonymous';
   });
