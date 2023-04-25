9942f51f10542983c9e6de4e38f189fb8b90e9d7
Adaptist <adaptist1@gmail.com>
2023-04-25 11:23:08 +0300

    6.08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+        if ([...formData.values()].some(value => !value.trim())) {
+            alert('Всі поля повинні бути заповнені');
+            return;
+        }
+    
+    const obj = {};
+
+    formData.forEach((value, key) => obj[key] = value);
+    console.log(obj);
+
+    loginForm.reset();
+});
